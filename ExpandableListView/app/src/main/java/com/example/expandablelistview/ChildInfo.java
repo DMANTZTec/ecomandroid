0673c4c4760424aba1aca4afe0b3b8eedbf2a02f
SriHarsha300399 <harsha.akavaram@gmail.com>
2019-04-25 11:49:19 +0530

    ExpandableListView Static

---
+++ ExpandableListView/app/src/main/java/com/example/expandablelistview/ChildInfo.java
@@ -0,0 +1,14 @@
+package com.example.expandablelistview;
+
+public class ChildInfo {
+
+    private String playerName = "";
+
+    public String getName() {
+        return playerName;
+    }
+
+    public void setName(String playerName) {
+        this.playerName = playerName;
+    }
+}
