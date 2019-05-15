464d35f4a247338d88ce6600f7285165bcbe64e0
SriHarsha300399 <harsha.akavaram@gmail.com>
2019-05-15 16:18:14 +0530

    Recycler View Dynamically

--- RVDynamically/app/src/main/java/com/android/rvdynamically/MainActivity.java
+++ RVDynamically/app/src/main/java/com/android/rvdynamically/MainActivity.java
@@ -1,13 +1,56 @@
 package com.android.rvdynamically;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.MenuItem;
+
+import com.android.rvdynamically.model.User;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private DrawerLayout mDrawerLayout;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    //catlog adapter
+    private RecyclerViewAdapter adapter;
+    private User user;
+    UserApp uapp;
+
+    private UserClient client;
+
+    public MainActivity() {
+
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // android.support.v7.widget.Toolbar toolbar = findViewById(R.id.)
+
+        uapp = (UserApp) getApplicationContext();
+        uapp.userClient.setContext(getApplicationContext());
+
+        ArrayList<User> userList = uapp.userClient.getUserFromDB();
+
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new GridLayoutManager(this, 2);
+
+        adapter = new RecyclerViewAdapter(this, userList);
+
     }
 }
